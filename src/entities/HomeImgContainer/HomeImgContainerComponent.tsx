b75eed70443bbd85a8f4b8dec805b00d21f6a47c
Aivanii <aivanellok@gmail.com>
2025-03-01 14:44:43 +0700

    added mainTitle

---
+++ src/entities/HomeImgContainer/HomeImgContainerComponent.tsx
@@ -0,0 +1,16 @@
+const HomeImgContainerComponent = () => {
+  return (
+    <div
+      className="h-screen mt-[-96px] z-10 flex items-center bg-linear-to-r from-black to-inherit
+    w-6xl pl-72" 
+    >
+      <h1 className="text-white text-6xl bg-ingerit text-center">
+        Доставка 
+        <div className="h-1 w-3xs bg-amber-500 mx-auto my-4"></div>
+        премиальных роллов
+      </h1>
+    </div>
+  );
+};
+
+export default HomeImgContainerComponent;
