baae4fcf62346ad7cae112c74e74c0e3c5f048e0
Aivanii <aivanellok@gmail.com>
2025-03-03 11:20:34 +0700

    update menu goods

--- src/entities/Menu/Menu.ts
+++ src/entities/Menu/Menu.ts
@@ -5,92 +5,62 @@ export const Menu = [
     price: 290,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с креветкой очень вкусный вариант для быстрого перекуса.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Ролл с лососем",
+    img: "https://s1.eda.ru/StaticContent/Photos/120131082911/160202105831/p_O.jpg",
+    price: 320,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с лососем — это классический выбор для любителей японской кухни.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Ролл с угрем",
+    img: "https://menu2go.ru/images/food/232/232_20220425152244_2244.jpg",
+    price: 350,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с угрем имеют насыщенный вкус и подаются с сладким соусом.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Вегетарианский ролл",
+    img: "https://distfood.ru/upload/medialibrary/60b/60b67506578d8e164fa5ac1a8b527795.jpg",
+    price: 250,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
-  },{
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
-    count: 4,
-    description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Вегетарианский ролл с овощами — отличный выбор для веганов.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Ролл с тунцом",
+    img: "https://chefpizza.ru/image/cache/catalog/sushi/roll/fila_tunec-550x550h.png",
+    price: 340,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с тунцом — идеальный вариант для любителей морепродуктов.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Ролл с крабом",
+    img: "https://cdn-ru4.foodpicasso.com/assets/2023/02/10/89297e3dbd5a308fb74ee12f31693088---jpg_1000x_103c0_convert.jpg",
+    price: 400,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с крабом — это деликатес, который стоит попробовать каждому.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Ролл с авокадо",
+    img: "https://www.distfood.ru/upload/medialibrary/79b/79bf87964d15da3cd62f8531e49a770b.jpg",
+    price: 270,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
-  },{
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
-    count: 4,
-    description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с авокадо — это легкий и питательный выбор для перекуса.",
   },
   {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
+    name: "Ролл с тофу",
+    img: "https://thapl-public.storage.yandexcloud.net/thapl-project481/img/CatalogItem/0e5c4b85df2a934907e01308fd2c0a08_thumb_80_1152_1152.jpg",
+    price: 260,
     count: 4,
     description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
-  },
-  {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
-    count: 4,
-    description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
-  },
-  {
-    name: "Ролл с креветкой",
-    img: "https://static.1000.menu/img/content/27657/rolly-s-krevetkami_1530116469_16_max.jpg",
-    price: 290,
-    count: 4,
-    description:
-      "Роллы с креветкой очень популярный и вкусный вариант для быстрого перекуса.",
+      "Роллы с овощами и тофу — это отличный вариант для вегетарианцев.",
   },
 ];
