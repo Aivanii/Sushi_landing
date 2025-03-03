e5fc51e28b418e431d3020fe70f51b21f0f3dd4a
Aivanii <aivanellok@gmail.com>
2025-03-03 13:54:48 +0700

    update carousel

--- src/features/FullGoodInfo.tsx
+++ src/features/FullGoodInfo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef } from "react";
 interface Props {
   isVisible: boolean;
   goodInfo: {
@@ -9,21 +9,29 @@ interface Props {
 }
 
 const FullGoodInfo: React.FC<Props> = ({ isVisible, goodInfo }) => {
- 
-    const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const currentSlide = useRef<number>(1);
 
-    const changeSlide = (value : number) =>{
-        if(currentSlide + value > goodInfo.imgs.length -1){
-            setCurrentSlide(0);
-        }else if(currentSlide + value < 0){
-            setCurrentSlide(goodInfo.imgs.length - 1);
-        }else{
-            setCurrentSlide(currentSlide + value);
-        }
-    };
-    
+  const changeSlide = (value: number) => {
+    if (currentSlide.current + value > goodInfo.imgs.length - 1) {
+      currentSlide.current = 0;
+    } else if (currentSlide.current + value < 0) {
+      currentSlide.current = goodInfo.imgs.length - 1;
+    } else {
+      currentSlide.current += value;
+    }
 
-    return (
+    const carousel = document.querySelector("#carousel") as HTMLElement;
+    let left: number = window.innerWidth / 2;
+    if (left < 200) left = 200;
+    if (left > 600) left = 600;
+    carousel.scrollTo({
+      top: 0,
+      left: left * currentSlide.current,
+      behavior: "smooth",
+    });
+  };
+
+  return (
     <div>
       {isVisible ? (
         <div
@@ -32,18 +40,25 @@ const FullGoodInfo: React.FC<Props> = ({ isVisible, goodInfo }) => {
         >
           <div className="flex row justify-center items-center">
             <div>
-              <div className="w-[clamp(200px,50vw,600px)] h-[clamp(150px,37.5vw,450px)] flex overflow-hidden">
-                  <img
-                    src={goodInfo.imgs[currentSlide]}
-                    key={goodInfo.imgs[currentSlide]}
-                    alt={goodInfo.name}
-                    className="w-[clamp(200px,50vw,600px)] h-[clamp(150px,37.5vw,450px)] object-cover my-auto rounded-sm shadow-lg"
-                  />
+              <div
+                className="w-[clamp(200px,50vw,600px)] h-[clamp(150px,37.5vw,450px)] flex overflow-hidden"
+                id="carousel"
+              >
+                {goodInfo.imgs.map((elem, index) => {
+                  return (
+                    <img
+                      key={elem}
+                      src={elem}
+                      alt={`${goodInfo.name} Фото #${index + 1}`}
+                      className="w-[clamp(200px,50vw,600px)] h-[clamp(150px,37.5vw,450px)] object-cover my-auto rounded-sm shadow-lg"
+                    />
+                  );
+                })}
               </div>
               <div className="absolute left-(50$) top-8 w-[clamp(200px,50vw,600px)] h-[clamp(150px,37.5vw,450px)]">
                 <div className="flex items-center justify-between h-full p-4">
                   <button
-                  onClick={() => changeSlide(1)}
+                    onClick={() => changeSlide(-1)}
                     className="cursor-pointer opacity-50 w-12 h-12 z-10 bg-black rounded-full flex
                 justify-center items-center flex-col hover:opacity-75 transition"
                   >
@@ -52,7 +67,7 @@ const FullGoodInfo: React.FC<Props> = ({ isVisible, goodInfo }) => {
                   </button>
 
                   <button
-                  onClick={() => changeSlide(-1)}
+                    onClick={() => changeSlide(1)}
                     className="cursor-pointer opacity-50 w-12 h-12 z-10 bg-black rounded-full flex
                 justify-center items-center flex-col hover:opacity-75 transition"
                   >
