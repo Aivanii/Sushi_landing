52ff41cabfabc3c86a0b9eb9460227be40ad6c69
Aivanii <aivanellok@gmail.com>
2025-03-05 14:35:03 +0700

    updated vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { md } from "vite-plugin-md";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), md()],
   root: "src/app",
 });
