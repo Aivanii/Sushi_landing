4cefda6a5687008c5b8d5d7fb05ae6f34a70bd54
Aivanii <aivanellok@gmail.com>
2025-03-05 14:51:52 +0700

    downdate vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { md } from "vite-plugin-md";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), md()],
+  plugins: [tailwindcss(), react()],
   root: "src/app",
 });
