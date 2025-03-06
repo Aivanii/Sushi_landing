a950025b14d3010b3924b8e13130abbf19f848a3
Aivanii <aivanellok@gmail.com>
2025-03-06 16:03:12 +0700

    updated responsiveness for footer

--- src/entities/Footer/FooterComponent.tsx
+++ src/entities/Footer/FooterComponent.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <footer className="mt-16 py-6 bg-white">
-      <div className="flex justify-center gap-8">
+      <div className="flex justify-center gap-8 flex-col sm:flex-row  p-4">
         <div>
           <div>
-            <h5 className="text-5xl">Roll Realms</h5>
-            <div className="flex">
+            <h5 className="text-4xl">Roll Realms</h5>
+            <div className="flex flex-wrap">
               <a href="#">
                 <img
                   src="https://img.icons8.com/?size=100&id=37325&format=png&color=000000"
@@ -41,15 +41,15 @@ const Footer = () => {
         </div>
         <div>
           <h5>Категории:</h5>
-          <ul className="flex flex-col h-full justify-evenly text-2xl">
+          <ul className="flex flex-col h-full justify-evenly text-2xl gap-4 sm:gap-0">
             <div>
-              <a href = "/#HomeImgContainer">Главная</a>
+              <a href="/#HomeImgContainer">Главная</a>
             </div>
             <div>
-              <a href = "/#menu">Меню</a>
+              <a href="/#menu">Меню</a>
             </div>
             <div>
-              <a href = "/#delivery">Доставка</a>
+              <a href="/#delivery">Доставка</a>
             </div>
           </ul>
         </div>
