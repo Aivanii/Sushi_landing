8476aaadf07b571699aab2137c31458bc25bb9bd
Aivanii <aivanellok@gmail.com>
2025-03-06 13:18:49 +0700

    updated responsive design for home img container component

--- src/entities/HomeImgContainer/HomeImgContainerComponent.tsx
+++ src/entities/HomeImgContainer/HomeImgContainerComponent.tsx
@@ -2,10 +2,15 @@ const HomeImgContainerComponent = () => {
   return (
     <div
       className="h-screen mt-[-96px] z-10 flex items-center bg-linear-to-r from-black to-inherit
-    w-6xl pl-72" 
-    id = "HomeImgContainer">
-      <h1 className="text-white text-6xl bg-ingerit text-center">
-        Доставка 
+    w-6xl pl-6 md:pl-20"
+      id="HomeImgContainer"
+    >
+      <h1
+        className="text-white text-2xl lg:text-6xl 
+        md:text-5xl bg-ingerit text-center
+      "
+      >
+        Доставка
         <div className="h-1 w-3xs bg-amber-500 mx-auto my-4"></div>
         премиальных роллов
       </h1>
