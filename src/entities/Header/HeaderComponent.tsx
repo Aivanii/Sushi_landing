8b6f7f6450b1d8831eb1b8899cfc9d98805607e5
Aivanii <aivanellok@gmail.com>
2025-03-06 13:26:49 +0700

    added not working burger btn

--- src/entities/Header/HeaderComponent.tsx
+++ src/entities/Header/HeaderComponent.tsx
@@ -1,19 +1,33 @@
 const Header = () => {
   return (
     <header>
-      <nav className="flex justify-around text-white py-8 text-2xl bg-black z-20">
+      <nav
+        className="flex justify-between p-4 md:justify-around text-white py-8 text-2xl bg-black z-20 items-center
+
+      "
+      >
         <div>
           <a href="/">Roll Realms</a>
         </div>
-        <div>
+        <div className="hidden md:visible">
           <a href="/#menu">Меню</a>
         </div>
-        <div>
+        <div className="hidden md:visible">
           <a href="/#delivery">Доставка</a>
         </div>
+        <div className="hidden md:visible">
+          <a href="/#contacts">+7 (923) 123-45-67</a>
+        </div>
         <div>
-        <a href="/#contacts">+7 (923) 123-45-67</a>
-          </div>
+          <button
+            id="burgerMenu"
+            className="flex gap-2 flex-col p-4 bg-white rounded-sm md:hidden cursor-pointer"
+          >
+            <div className="w-8 h-1 bg-black rounded-sm"></div>
+            <div className="w-8 h-1 bg-black rounded-sm"></div>
+            <div className="w-8 h-1 bg-black rounded-sm"></div>
+          </button>
+        </div>
       </nav>
     </header>
   );
