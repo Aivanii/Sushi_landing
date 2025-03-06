942af13826655d46c004b1ba817b0d37b847c75a
Aivanii <aivanellok@gmail.com>
2025-03-06 16:00:57 +0700

    updated responsiveness for contacts component

--- src/entities/Contacts/ContactsComponent.tsx
+++ src/entities/Contacts/ContactsComponent.tsx
@@ -1,9 +1,11 @@
 const ContactsComponent = () => {
   return (
-    <div className="mt-16 py-6 bg-white" id = "contacts">
-      <h2 className="text-6xl text-center">Контакты</h2>
+    <div className="mt-16 py-6 bg-white" id="contacts">
+      <h2 className="text-2xl sm:text-4xl md:text-5xl  lg:text-6xl text-center">
+        Контакты
+      </h2>
       <div className="flex justify-center items-center flex-col">
-        <div className="flex justify-around">
+        <div className="flex justify-around flex-col sm:flex-row items-center sm:gap-8">
           <div className="w-1/5 flex flex-col text-center justify-center items-center">
             <img
               src="https://img.icons8.com/?size=100&id=9659&format=png&color=000000"
@@ -33,7 +35,7 @@ const ContactsComponent = () => {
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230467.43830979816!2d-9.36244083497454!3d53.546918960565606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48448fff30655681%3A0xb49ba57568fac0b6!2z0JfQsNC80L7QuiDQrdGI0YTQvtGA0LQ!5e0!3m2!1sru!2sru!4v1741146917770!5m2!1sru!2sru"
             style={{ border: 0 }}
-            className="h-80 w-dvw"
+            className="h-80 w-dvw max-w-5xl my-auto"
             allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
