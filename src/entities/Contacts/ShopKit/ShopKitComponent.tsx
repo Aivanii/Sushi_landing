c334c147a844dc61e194bd86736835bdfa0249af
Aivanii <aivanellok@gmail.com>
2025-03-07 11:22:01 +0700

    fixed styles for shopkit btn

--- src/entities/Contacts/ShopKit/ShopKitComponent.tsx
+++ src/entities/Contacts/ShopKit/ShopKitComponent.tsx
@@ -31,20 +31,24 @@ const ShopKitComponent = () => {
   0);
   return (
     <>
-      <div
-        className="fixed bottom-2 right-2 rounded-full
-      w-24 h-24 bg-white flex justify-center items-center border-2 z-10"
-      >
-        <img
-          className="w-20 h-20"
-          src="https://img.icons8.com/?size=100&id=0DBkCUANmgoQ&format=png&color=000000"
-          alt="Корзина"
-        />
-        <span className="absolute top-0 right-0 z-20 bg-amber-600 w-8 h-8 rounded-full
-        flex justify-center  items-center text-center text-white border-2 border-black">
-          {totalGoodsInShopKit > 0 &&
-            (totalGoodsInShopKit > 99 ? "99+" : totalGoodsInShopKit)}
-        </span>
+      <div className="fixed bottom-2 right-2 ">
+        <button
+          className="rounded-full
+      w-20 h-20 bg-white flex justify-center items-center border-2 z-10 cursor-pointer"
+        >
+          <img
+            className="w-16 h-16"
+            src="https://img.icons8.com/?size=100&id=0DBkCUANmgoQ&format=png&color=000000"
+            alt="Корзина"
+          />
+          <span
+            className="absolute top-0 right-0 z-20 bg-amber-600 w-8 h-8 rounded-full
+        flex justify-center  items-center text-center text-white border-2 border-black"
+          >
+            {totalGoodsInShopKit > 0 &&
+              (totalGoodsInShopKit > 99 ? "99+" : totalGoodsInShopKit)}
+          </span>
+        </button>
       </div>
     </>
   );
