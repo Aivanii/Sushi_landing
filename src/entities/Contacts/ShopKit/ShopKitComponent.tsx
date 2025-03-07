aaeebc733194ae9aacd53c9d354aa72995739eec
Aivanii <aivanellok@gmail.com>
2025-03-07 10:43:46 +0700

    added shopKit visual btn

--- src/entities/Contacts/ShopKit/ShopKitComponent.tsx
+++ src/entities/Contacts/ShopKit/ShopKitComponent.tsx
@@ -1,5 +1,18 @@
 const ShopKitComponent = () => {
-  return <div>shopkit</div>;
+  return (
+    <>
+      <div
+        className="fixed bottom-2 right-2 rounded-full
+      w-24 h-24 bg-white flex justify-center items-center border-2"
+      >
+        <img
+          className="w-20 h-20"
+          src="https://img.icons8.com/?size=100&id=0DBkCUANmgoQ&format=png&color=000000"
+          alt="Корзина"
+        />
+      </div>
+    </>
+  );
 };
 
 export default ShopKitComponent;
