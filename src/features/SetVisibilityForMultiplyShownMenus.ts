c3c48198cbbff618e8781c13d869fde13acc1706
Aivanii <aivanellok@gmail.com>
2025-03-09 20:25:11 +0700

    deleted testcase

--- src/features/SetVisibilityForMultiplyShownMenus.ts
+++ src/features/SetVisibilityForMultiplyShownMenus.ts
@@ -1,5 +1,4 @@
 const SetVisibilityForMultiplyShownMenus = () => {
-  console.log(123)
   const elements = document.querySelectorAll(".zoom-in");
   elements.forEach((element) => {
     element.classList.add("hidden");
