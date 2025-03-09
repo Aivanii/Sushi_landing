2fdcdc2e4373043be29eb902f1b6428ef2020635
Aivanii <aivanellok@gmail.com>
2025-03-09 19:17:35 +0700

    deleted react-router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,9 @@
 //pages
 import { HomePage } from "./index.ts";
-import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+      <HomePage />
   );
 }
 
