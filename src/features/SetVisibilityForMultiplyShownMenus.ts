873606c8abf859aaf15a0f5eb355649ea59fc71a
Aivanii <aivanellok@gmail.com>
2025-03-09 19:08:39 +0700

    deleted unused files

--- src/features/SetVisibilityForMultiplyShownMenus.ts
+++
@@ -1,9 +0,0 @@
-const SetVisibilityForMultiplyShownMenus = () => {
-  console.log(123)
-  const elements = document.querySelectorAll(".zoom-in");
-  elements.forEach((element) => {
-    element.classList.add("hidden");
-  });
-};
-
-export default SetVisibilityForMultiplyShownMenus;
